b50d697f3ceb28690aa2dfc400f2f16b164073ee
Fede <fedelll@gmail.com>
2022-11-28 10:09:08 +0100

    event styles refactor

--- components/calendar/dayCell.js
+++ components/calendar/dayCell.js
@@ -1,37 +1,39 @@
 import format from 'date-fns/format';
 import styles from './calendar.module.scss';
 
+function useCellBodyClass(eventType, eventsAtDay, isOngoingEvent) {
+  let cellBodyClass = styles.cellBody;
+
+  if (eventType === 'START') {
+    cellBodyClass += ' ' + styles.start;
+  } else if (eventType === 'MIDDLE') {
+    cellBodyClass += ' ' + styles.middle;
+  } else if (eventType === 'END') {
+    cellBodyClass += ' ' + styles.end;
+  } else if (eventsAtDay) {
+    cellBodyClass += ' ' + styles.withEvent;
+  } else if (isOngoingEvent) {
+    cellBodyClass += ' ' + styles.ongoing;
+  }
+
+  return cellBodyClass;
+}
+
 export default function DayCell(props) {
   const { day, eventsAtDay, register, isEditMode, isOngoingEvent } = props;
   const event = (eventsAtDay && eventsAtDay[0]) || {};
   const { title, type, id } = event;
 
-  let cellBodyClass = styles.cellBody;
-
-  let cellBodyStyle = { width: '50px' };
-  if (eventsAtDay) {
-    cellBodyStyle = {};
-  }
-
-  if (type === 'START') {
-    cellBodyClass += ' ' + styles.start;
-  } else if (type === 'MIDDLE') {
-    cellBodyClass += ' ' + styles.middle;
-  } else if (type === 'END') {
-    cellBodyClass += ' ' + styles.end;
-  } else if (!eventsAtDay && isOngoingEvent) {
-    cellBodyClass += ' ' + styles.ongoing;
-    cellBodyStyle = { width: '72px' };
-  }
-
   const shouldShowEvent = isOngoingEvent || eventsAtDay;
+
+  const cellBodyClass = useCellBodyClass(type, eventsAtDay, isOngoingEvent);
 
   return (
     <div className={styles.cell}>
       <div className={styles.cellHeader}>
         {format(day, 'EE')} <span>{format(day, 'd')}</span>
       </div>
-      <div className={cellBodyClass} style={cellBodyStyle}>
+      <div className={cellBodyClass}>
         {shouldShowEvent && (
           <div className={styles.event}>
             {eventsAtDay && (
