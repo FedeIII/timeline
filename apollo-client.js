4367ce715c9f05f829850173a1f3bd9fc6ef5443
Fede <fedelll@gmail.com>
2022-12-22 15:45:06 +0100

    remote db

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,8 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    uri: 'http://localhost:8080/graphql',
+    // uri: 'http://localhost:8080/graphql',
+    uri: 'https://timeline-service.herokuapp.com/graphql',
     credentials: 'same-origin',
   }),
   cache: new InMemoryCache(),
