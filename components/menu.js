80c0928b9e652790b6d665fbcc55ddc6b8fcb9c3
Fede <fedelll@gmail.com>
2022-12-24 03:55:48 +0100

    twitter login

--- components/menu.js
+++ components/menu.js
@@ -4,20 +4,65 @@ import Link from 'next/link';
 import SettingsIcon from './settings-icon';
 import utilStyles from '../styles/utils.module.scss';
 import styles from './menu.module.scss';
+import { useContext, useEffect, useMemo, useState } from 'react';
+import EnvContext from '../contexts/envContext';
+import UserContext from '../contexts/userContext';
+
+function useTwitterOauthUrl(redirect_uri, client_id) {
+  return useMemo(() => {
+    const qs = new URLSearchParams({
+      redirect_uri,
+      client_id,
+      state: 'state',
+      response_type: 'code',
+      code_challenge: 'y_SfRG4BmOES02uqWeIkIgLQAlTBggyf_G7uKT51ku8',
+      code_challenge_method: 'S256',
+      scope: ['tweet.read', 'tweet.write', 'users.read'].join(' '),
+    }).toString();
+    return `https://twitter.com/i/oauth2/authorize?${qs}`;
+  }, [redirect_uri, client_id]);
+}
 
 export default function Menu({ children }) {
+  const { TWITTER_REDIRECT_URI, TWITTER_CLIENT_ID, ME_URL } =
+    useContext(EnvContext);
+  const twitterOauthUrl = useTwitterOauthUrl(
+    TWITTER_REDIRECT_URI,
+    TWITTER_CLIENT_ID
+  );
+
+  const [user, setUser] = useContext(UserContext);
+  useEffect(() => {
+    (async function () {
+      try {
+        if (!ME_URL) return;
+        const response = await fetch(ME_URL, { credentials: 'include' });
+        const user = await response.json();
+        if (user) setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [ME_URL]);
+
   return (
     <>
-      <Link href="/" className={styles.profileLink}>
-        <Image
-          priority
-          src="/images/profile.jpg"
-          className={utilStyles.borderCircle}
-          height={32}
-          width={32}
-          alt=""
-        />
-      </Link>
+      <div className={styles.profile}>
+        {TWITTER_CLIENT_ID && (
+          <Link href={twitterOauthUrl} className={styles.profileLink}>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={32}
+              width={32}
+              alt=""
+            />
+          </Link>
+        )}
+
+        {user && <span className={styles.userName}>{user.name}</span>}
+      </div>
 
       {children}
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react';
+import EnvContext from '../contexts/envContext';
+import UserContext from '../contexts/userContext';
 import '../styles/global.scss';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+async function fetchEnvVars(setEnvVars) {
+  const response = await fetch('https://timeline-service.herokuapp.com/handshake');
+  // const response = await fetch('http://127.0.0.1:8080/handshake');
+  setEnvVars(await response.json());
+}
+
+function useEnvVars() {
+  const [envVars, setEnvVars] = useState({});
+
+  useEffect(() => {
+    fetchEnvVars(setEnvVars);
+  }, []);
+
+  return envVars;
+}
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  const envVars = useEnvVars();
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      <EnvContext.Provider value={envVars}>
+        <Component {...pageProps} />
+      </EnvContext.Provider>
+    </UserContext.Provider>
+  );
 }
