a090e31069d83d9ec2cf6bfca076a6e380786cd7
Fede <fedelll@gmail.com>
2022-12-24 15:17:59 +0100

    credentials

--- apollo-client.js
+++ apollo-client.js
@@ -3,9 +3,9 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    // uri: 'http://localhost:8080/graphql',
+    // uri: 'http://127.0.0.1:8080/graphql',
     uri: 'https://timeline-service.herokuapp.com/graphql',
-    credentials: 'same-origin',
+    credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
